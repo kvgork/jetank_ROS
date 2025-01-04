1f183f1637f67a0ee881b31fd7074f3e5cf68f7b
Koen van Gorkom <gorkom.projects@gmail.com>
2025-01-04 13:05:21 +0100

    fix stereo camera

--- src/camera.py
+++ src/camera.py
@@ -26,7 +26,7 @@ class StereoCamera(SingletonConfigurable):
     right_sensor_id = traitlets.Integer(default_value=1).tag(config=True)
 
     def __init__(self, *args, **kwargs):
-        super(StereoCameraCamera, self).__init__(*args, **kwargs)
+        super(StereoCamera, self).__init__(*args, **kwargs)
         self.left_value = np.empty((self.height, self.width, 3), dtype=np.uint8)
         self.right_value = np.empty((self.height, self.width, 3), dtype=np.uint8)
 
@@ -44,8 +44,8 @@ class StereoCamera(SingletonConfigurable):
         self.rate = rospy.Rate(10)  # 10 Hz
 
         try:
-            self.left_cap = cv2.VideoCapture(self._gst_str(sensor_id=left_sensor_id), cv2.CAP_GSTREAMER)
-            self.right_cap = cv2.VideoCapture(self._gst_str(sensor_id=right_sensor_id), cv2.CAP_GSTREAMER)
+            self.left_cap = cv2.VideoCapture(self._gst_str(sensor_id=self.left_sensor_id), cv2.CAP_GSTREAMER)
+            self.right_cap = cv2.VideoCapture(self._gst_str(sensor_id=self.right_sensor_id), cv2.CAP_GSTREAMER)
             
             left_re, left_image = self.left_cap.read()
             right_re, right_image = self.right_cap.read()
@@ -66,7 +66,7 @@ class StereoCamera(SingletonConfigurable):
         atexit.register(self.stop)
 
                 
-    def _gst_str(self):
+    def _gst_str(self, sensor_id):
         return (
             "nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, "
             "format=(string)NV12, framerate=%d/1 ! nvvidconv ! video/x-raw, "
@@ -76,17 +76,17 @@ class StereoCamera(SingletonConfigurable):
     
     def start(self):
         if not self.left_cap.isOpened():
-            self.left_cap.open(self._gst_str(sensor_id=left_sensor_id), cv2.CAP_GSTREAMER)
+            self.left_cap.open(self._gst_str(sensor_id=self.left_sensor_id), cv2.CAP_GSTREAMER)
         if not self.right_cap.isOpened():
-            self.tight_cap.open(self._gst_str(sensor_id=right_sensor_id), cv2.CAP_GSTREAMER)
+            self.tight_cap.open(self._gst_str(sensor_id=self.right_sensor_id), cv2.CAP_GSTREAMER)
 
         while not rospy.is_shutdown():
             left_re, left_image = self.left_cap.read()
             right_re, right_image = self.right_cap.read()
             if left_re and right_re:
                 # Publish the image
-                self.left_image_pub.publish(self.frame_to_ros_image(frame_left, "left"))
-                self.right_image_pub.publish(self.frame_to_ros_image(frame_right, "right"))
+                self.left_image_pub.publish(self.frame_to_ros_image(left_image, "left"))
+                self.right_image_pub.publish(self.frame_to_ros_image(right_image, "right"))
                 rospy.loginfo("Published stereo camera frames.")
             else:
                 rospy.logwarn("Failed to capture frames from both cameras.")
